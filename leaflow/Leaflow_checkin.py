2819b5471007233e114a5080d898a8d9c01a4ab3
greenwave1987 <48582745+greenwave1987@users.noreply.github.com>
2026-01-09 17:15:02 +0800

    Update Leaflow_checkin.py

--- leaflow/Leaflow_checkin.py
+++ leaflow/Leaflow_checkin.py
@@ -33,14 +33,13 @@ DASHBOARD_URL = "https://leaflow.net/dashboard"
 checkin_url = "https://checkin.leaflow.net"
 main_site = "https://leaflow.net"
 headers={
-    'User-Agent': self.config['settings']['user_agent'],
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'DNT': '1',
-    'Connection': 'keep-alive',
-    'Upgrade-Insecure-Requests': '1',
-        }
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8",
+    "DNT": "1",
+    "Connection": "keep-alive",
+    "Upgrade-Insecure-Requests": "1"
+}
 # ================= 账号 / Cookies =================
 
 def load_accounts():
